90dfdb0c5926d99a5b6b063530fc589508c0e6fe
gnanasekart <gnanasekar69@hotmail.com>
2024-08-31 21:55:08 +0530

    new commit

---
+++ MainTest.java
@@ -0,0 +1,8 @@
+public class MainTest {
+
+    public static void main(String[] args) {
+        String[] arr = {"apple", "banana", "cherry", "date", "elderberry"};
+        System.out.println(arr.length);
+    }
+    
+}
