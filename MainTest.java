189ff949f9fbdcec53422ad3b0149ef5ddb1191d
Gnana sekar T <64648019+gnanasekart@users.noreply.github.com>
2024-08-31 22:27:12 +0530

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -3,6 +3,7 @@ public class MainTest {
     public static void main(String[] args) {
         String[] arr = {"apple", , "cherry", "date", "elderberry"};
         System.out.println(arr.length);
+        //Update
         //print new output here
     }
 }
