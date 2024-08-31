4fa6747b6feaa127b5c63c39cffd87168d0c1925
gnanasekart <gnanasekar69@hotmail.com>
2024-08-31 21:58:25 +0530

    new commit after edit

--- MainTest.java
+++ MainTest.java
@@ -3,6 +3,8 @@ public class MainTest {
     public static void main(String[] args) {
         String[] arr = {"apple", "banana", "cherry", "date", "elderberry"};
         System.out.println(arr.length);
+
+        //print new output
     }
     
 }
